82d5ed33d055a00b2cbb6d0eed80baecd52d8c04
Mayank Maloo <mayankmaloo3@gmail.com>
2022-12-17 23:31:56 +0530

    Added Merge Sort File successfully

---
+++ Sorting/merge sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+void MergeSort(int arr[],int low,int high){
+    if(low<high){
+        int mid = (low+high)/2;
+
+        MergeSort(arr,low,mid);
+        MergeSort(arr,mid+1,high);
+        Merge(arr,low,mid,high);
+
+
+    }
+}
+
+void Merge(int arr[],int low,int mid,int high){
+    int i=low,j=mid+1,k=low;
+    int temp[100000];
+
+    while(i<=mid && j<=high){
+        if(arr[i]<arr[j])temp[k++] =arr[i++];
+
+
+        else
+        temp[k++] = arr[j++];
+    }
+
+    
+    if(i>mid)//Means we have completed first half now we have only second half remaining{
+        while(j<= high)temp[k++] =arr[j++];
+    
+    
+    else{
+            while(i<=mid)temp[k++] = arr[i++];
+    }
+
+
+   for(int k=low;k<=high;k++){
+    arr[k]=temp[k]; // This will move sorted array from temp array to original array
+   }
+
+
+
+}
